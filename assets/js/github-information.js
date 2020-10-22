010f14448aa1d5a1864f2c4cac57d81f81e49395
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2020-10-22 15:53:09 +0000

    added emailjs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -57,7 +57,10 @@ const fetchGitHubInformation = (event) => {
           $("#gh-user-data").html(
             `<h2>No info found for user ${username}</h2>`
           );
-        } else {
+        } else if(errorResponse.status === 403){
+            let resetTime = new Date(errorResponse.getResponseHeader("X-RateLimit-Reset")*1000);
+            $("#gh-user-data").html(`<h4>Too many request please wait untill ${resetTime.toLocaleTimeString()}</h4>`)
+        }else {
           console.log(errorResponse);
           $("#gh-user-data").html(
             `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
